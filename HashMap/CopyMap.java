1e0b3104cb56802591f4915e595871a6291f4159
Madhvi Mishra <madhvimishra2207@gmail.com>
2026-07-11 23:54:49 +0530

    HashMap Functions

--- HashMap/CopyMap.java
+++ HashMap/CopyMap.java
@@ -10,11 +10,25 @@ public class CopyMap {
         map.put('V', 5);
         map.put('X', 10);
 
-        HashMap<Character,Integer> map1 = new HashMap<>(Map.of(
+        HashMap<Character,Integer> m = new HashMap<>(Map.of(
             'L',50,
             'C',100
         ));
-        map.putAll(map1);
+        map.putAll(m);
         System.out.println(map);
+
+        // Overwriting Existing Key
+
+        HashMap<String, Integer> map1 = new HashMap<>();
+        map1.put("Apple", 10);
+        map1.put("Banana", 20);
+
+        HashMap<String, Integer> map2 = new HashMap<>();
+        map2.put("Banana", 50);
+        map2.put("Orange", 30);
+
+        map1.putAll(map2);
+
+        System.out.println(map1);
     }
 }
--- HashMap/MAPFUNCTIONS.java
+++ HashMap/MAPFUNCTIONS.java
@@ -1,6 +1,5 @@
 package HashMap;
 
-import java.util.HashMap;
 import java.util.*;
 public class MAPFUNCTIONS {
     public static void main(String args[])
@@ -10,6 +9,17 @@ public class MAPFUNCTIONS {
         map.put('I', 1);
         map.put('V', 5);
         map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        for(Map.Entry<Character, Integer> e : map.entrySet())
+        {
+            System.out.println(e.getKey() + " : " + e.getValue());
+        }
+
+        System.out.println("Size : " + map.size());
 
         if(map.containsKey('I'))
         {
@@ -25,6 +35,9 @@ public class MAPFUNCTIONS {
         System.out.println("Input Key : ");
         char ch =sc.next().charAt(0);
         System.out.println(map.get(ch));
+        map.remove(ch);
+        System.out.println(map);
+
         sc.close();
     }
     
