56e4aa4270ce902aa629c9eac51a2bfa961af3ea
Madhvi Mishra <madhvimishra2207@gmail.com>
2026-07-12 00:40:11 +0530

    String Builder Initialization

---
+++ StringBuilder/Functions.java
@@ -0,0 +1,10 @@
+package StringBuilder;
+
+public class Functions {
+    public static void main(String args[])
+    {
+        StringBuilder s = new StringBuilder("Hello");
+        s.append(" World");
+        System.out.println(s);
+    }
+}
